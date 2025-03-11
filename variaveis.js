eae6fdd790a62e9ea8c7d88e3f01c0c9cda46d2a
Thomas Max Delling - 82329560 <82329560@ulife.com.br>
2025-03-11 10:12:38 -0300

    trabalhando com variaveis

---
+++ variaveis.js
@@ -0,0 +1,22 @@
+let nome = "Thomas"; //mutavel, sempre entre aspas, string
+const PI = 3.14; // nao mutavel, double
+var idade = 39; // nao utilizar, mutavel, var é global (visualizadas, mudadas em qualque parte do codigo)
+let ativo = true; //valor logico, false ou true
+
+let num = Number("42")
+let flost = parseFloat("5.7") //converte numero para string
+let inteiro = parseInt("10.5") //deixa inteiro
+
+let str = String(100)
+let outraStr = (42).toString()// converter  numero para string
+
+console.log(nome) //comando pra mostrar as variaveis
+console.log(inteiro)
+console.log(flost)
+
+if (true) {
+
+    
+}
+
+
