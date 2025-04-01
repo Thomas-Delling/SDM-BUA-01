22b21b082b58ba54501af2028d688e382096d64b
Thomas Max Delling - 82329560 <82329560@ulife.com.br>
2025-04-01 11:32:32 -0300

    atualizando

--- monolitos.js
+++ monolitos.js
@@ -1,20 +1,34 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-let usuarios = []
-let pedidos = []
+let usuarios = [];
+let pedidos = [];
 
-// rota para cadastro de usuario
-
+// Rota para cadastro de usuario
 app.post("/usuarios", (req, res) => {
     const usuario = req.body;
-    usuarios.push(usuario)
-    res.send({message: "usuario cadastrado com sucesso", usuario})
-})
+    usuarios.push(usuario);
+    res.send({ message: "Usuário cadastrado com sucesso", usuario });
+});
 
+// Rota para consulta de dados de usuários
 app.get("/dados", (req, res) => {
-    res.send({usuarios})
-})
-app.listen(3000, () => console.log("servidor monolitico on, na porta 3000"))
+    res.send({ usuarios });
+});
+
+// Rota para cadastro de pedidos
+app.post("/pedidos", (req, res) => {
+    const pedido = req.body;
+    pedidos.push(pedido);
+    res.send({ message: "Pedido cadastrado com sucesso", pedido });
+});
+
+// Rota para consulta de pedidos (corrigido)
+app.get("/pedidos", (req, res) => {  // Alterei de /pedido para /pedidos
+    res.send({ pedidos });  // Retorna todos os pedidos cadastrados
+});
+
+// Inicia o servidor na porta 3000
+app.listen(3000, () => console.log("Servidor monolítico ON, na porta 3000"));
